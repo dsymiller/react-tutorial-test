c722915ee8f8d3638289c43b577a07dd729f0b2f
Derek Miller <dsymiller@gmail.com>
2020-09-22 16:14:03 -0400

    Updates

---
+++ precache-manifest.cecf58b218932e6f38c45acbfedec1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d006b84269e673412e24dc352624cb5",
+    "url": "/react-tutorial-test/index.html"
+  },
+  {
+    "revision": "32566606d643f8a1976c",
+    "url": "/react-tutorial-test/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "cdae9f93682589905339",
+    "url": "/react-tutorial-test/static/js/2.deec75ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial-test/static/js/2.deec75ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32566606d643f8a1976c",
+    "url": "/react-tutorial-test/static/js/main.6058394a.chunk.js"
+  },
+  {
+    "revision": "682152a0fe9992f593f2",
+    "url": "/react-tutorial-test/static/js/runtime-main.8c3846e0.js"
+  }
+]);
